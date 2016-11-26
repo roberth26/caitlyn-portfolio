5dd8c1bf4cdd2f222e92e13528ff339d69506efd
roberth26 <robbiehall26@gmail.com>
2016-11-25 18:59:46 -0800

    sections. sass. mixins, variables, logo

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 		<style><?= file_get_contents( 'css/index.min.css' ) ?></style>
 	</head>
 	<body>
-
-		<?php include( 'footer.php' ) ?>
+		<?php include( 'sections/start/start.php' ) ?>
+		<?php include( 'sections/about/about.php' ) ?>
+		<?php include( 'sections/footer/footer.php' ) ?>
 		<script><?= file_get_contents( 'js/dist/index.min.js' ) ?></script>
 	</body>
 </html>
---
+++ sections/start/start.php
@@ -0,0 +1,5 @@
+<section class="start">
+	<div class="start__container">
+		<?= file_get_contents( 'assets/logo.svg' ) ?>
+	</div>
+</section>
